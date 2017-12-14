eb9f943cb078cd2a090c57204ed9be9c6a8e5844
dkati <dkatikaridis@gmail.com>
2017-12-14 12:25:04 +0200

    soong:Add Uses_non_treble_camera variable
    Change-Id: I25b046b1579dad9ddb43eda6e4c7d2ce84a2b6a1

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -7,5 +7,6 @@ lineage_soong:
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
 	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
